8ff3bbef36e7ff20e7660ea1f7408b420fbe5781
jjbmarababol <jjbmarababol@gmail.com>
2022-06-03 14:51:11 +0800

    [CH-1] Counter Reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { counterReducer } from './counter.reducer';
-
+import { MyCounterComponent } from './my-counter/my-counter.component';
 @NgModule({
   imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyCounterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
